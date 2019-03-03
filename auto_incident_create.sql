b8eb12f04ae0a62c72c0d8f367f7d6755ef23a14
Taras Kaminsky <taras.kami@hotmail.com>
2019-03-02 19:17:38 -0500

    Create auto_incident_create.sql

---
+++ auto_incident_create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE auto_incident (
+    -- Put attributes here 
+);
+
+CREATE TABLE three_one_one_request (
+    -- Put attributes here
+);
+
+-- This is one way I see of creating a table. Could also look at how it's done for baseball data in HW3
+-- COPY auto_incident( Put attributes here )
+-- FROM 'NYPD_Motor_Vehicle_Collisions.csv' DELIMITER ',' CSV HEADER;
