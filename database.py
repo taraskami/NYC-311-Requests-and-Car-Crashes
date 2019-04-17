556df9b7a70556be095bddf5927cd5698f415037
taraskami <taras.kami@hotmail.com>
2019-04-17 16:36:25 -0400

    Template for the rest of the functions

--- database.py
+++ database.py
@@ -7,3 +7,30 @@ def query_fn1(street_name, boro_name):
     cur.execute("SELECT COUNT(*) FROM crashes_location WHERE street = %s OR crossstreet = %s", (street_name, street_name, ))
     result = cur.fetchall()
     return result[0][0]
+
+def query_fn2():
+    return
+
+def query_fn3():
+    return
+
+def query_fn4():
+    return
+
+def query_fn5():
+    return
+
+def query_fn6():
+    return
+
+def query_fn7():
+    return
+
+def query_fn8():
+    return
+
+def query_fn9():
+    return
+
+def query_fn10():
+    return
