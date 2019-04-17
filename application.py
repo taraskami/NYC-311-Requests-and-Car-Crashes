dd79a745ec6a79dbc5e45f3f243f4b3cdbcdf582
taraskami <taras.kami@hotmail.com>
2019-04-17 13:36:51 -0400

    The ultimate sacrifice

--- application.py
+++ application.py
@@ -1,2 +1,6 @@
 # For starting application, calling function to populate tables,
-# Calling query functions created in database.py, and printing to cmd 
+# Calling query functions created in database.py, and printing to cmd
+
+import load_data #Creates tables and populates them.
+
+print "Hello! Sacrifice your children."
