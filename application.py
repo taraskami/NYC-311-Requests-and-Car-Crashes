937e3270cdc12f18f0f2851b517ae27f8090b36b
taraskami <taras.kami@hotmail.com>
2019-04-17 16:28:01 -0400

    Query 1 done

--- application.py
+++ application.py
@@ -2,31 +2,35 @@
 # Calling query functions created in database.py, and printing to cmd
 
 import sys
-import load_data #Creates tables and populates them.
-import database
 
 admin_mode = False #Admin mode lets the user write their own query.
-print sys.argv
-if len(sys.argv) > 1:
-    if sys.argv[1] == "-admin":
+run_sql = True #If tables are already created, no need to redo each time.
+for i in range(len(sys.argv)):
+    if sys.argv[i] == "-admin":
         admin_mode = True
+    elif sys.argv[i] == "-preinstalled":
+        run_sql = False
+if run_sql:
+    import load_data #Creates tables and populates them.
+from database import *
 
 if not admin_mode:
     print "Welcome to the NYC 311 Requests and Car Crashes application."
     print "What would you like to examine?"
     print """1: [Street and Number of Crashes]    2: [Potential Causes of Crashes]
-3: [Crashes on the Highway]           4: [Crash Deaths per Street, ZIP, or Boro]
+3: [Crashes on the Highway]           4: [Crash Deaths per Street, ZIP, or Borough]
 5: [Multi-Car Crashes]                6: [Crashes on Street in a Period of Time]
 7: [Potential Crashes linked to Complaint Type]
 8: [Potential Crashes linked to Complaint Reason]
 9: [All Crashes]                      10: [All Requests]"""
 
     topic_index = input()
-    if topic_index == '1':
+    if topic_index == 1:
         print "[Street and Number of Crashes]"
-        print "Input the street you'd like to examine"
-        print "(Tips: you may have to try variations of spellings for street types (i.e. parkway and pkwy)"
-        street_name = input()
+        street_name = raw_input("Input the street you'd like to examine\nTips: you may have to try variations of spellings for street types (i.e. parkway and pkwy)")
+        print "Specify a borough (Optional, may leave blank)"
+        boro_name = raw_input()
+        result = query_fn1(street_name, boro_name)
+        print str(result) + " crashes on this street."
     else:
-        print "..."
-    
+        print "Invalid"
--- database.py
+++ database.py
@@ -1,1 +1,9 @@
+import psycopg2
+conn = psycopg2.connect("host=localhost dbname=requests_and_crashes user=requests_and_crashes password = requests_and_crashes ")
+cur = conn.cursor()
+
 # Functions to query data
+def query_fn1(street_name, boro_name):
+    cur.execute("SELECT COUNT(*) FROM crashes_location WHERE street = %s OR crossstreet = %s", (street_name, street_name, ))
+    result = cur.fetchall()
+    return result[0][0]
