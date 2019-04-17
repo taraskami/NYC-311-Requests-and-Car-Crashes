98dd317bd679b02f85f7154f266d4fe66990eb19
taraskami <taras.kami@hotmail.com>
2019-04-17 14:56:58 -0400

    printed options

--- application.py
+++ application.py
@@ -12,6 +12,10 @@ if len(sys.argv) > 1:
 
 if not admin_mode:
     print "Welcome to the NYC 311 Requests and Car Crashes application."
-    print "Would you like to examine crashes' location (1), time (2), contributing factor (3), or vehicle type (4)?"
-    crashes_detail = input()
-    
+    print "What would you like to examine?"
+    print """1: [Address and Number of Crashes]    2: [Potential Causes of Crashes]
+3: [Crashes on the Highway]           4: [Crash Deaths per Street, ZIP, or Boro]
+5: [Multi-Car Crashes]                6: [Crashes in a Period of Time]
+7: [Potential Crashes linked to Complaint Type]
+8: [Potential Crashes linked to Complaint Reason]
+9: [All Crashes]                      10: [All Requests]"""
